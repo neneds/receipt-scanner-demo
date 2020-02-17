c7b8cc425a7ab523a14a84dba104946796986b24
Dennis <dennis.rodrigues@aisdigital.com.br>
2020-02-17 17:00:52 -0300

    :cool: Adjusted code for load from library

--- BusinessCompanion/DocumentScanningViewController.swift
+++ BusinessCompanion/DocumentScanningViewController.swift
@@ -15,12 +15,14 @@ class DocumentScanningViewController: UIViewController, UINavigationControllerDe
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     static let otherContentsIdentifier = "otherContentsVC"
+    let scanner = VisionKeywordScanner()
     
     var resultsViewController: (UIViewController & RecognizedTextDataSource)?
     var textRecognitionRequest = VNRecognizeTextRequest()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
     @IBAction func scan(_ sender: UIControl) {
@@ -48,12 +50,12 @@ extension DocumentScanningViewController: VNDocumentCameraViewControllerDelegate
         resultsViewController = storyboard?.instantiateViewController(withIdentifier: DocumentScanningViewController.otherContentsIdentifier) as? (UIViewController & RecognizedTextDataSource)
         
         self.activityIndicator.startAnimating()
+        
         controller.dismiss(animated: true) {
             DispatchQueue.global(qos: .userInitiated).async {
                 for pageNumber in 0 ..< scan.pageCount {
                     let image = scan.imageOfPage(at: pageNumber)
-                    let scanner = VisionKeywordScanner()
-                    scanner.recognize(source: image) { (candidate, error) in
+                    self.scanner.recognize(source: image) { (candidate, error) in
                         if error != nil {
                             print("Error: \(error?.localizedDescription ?? "")")
                         }
@@ -74,21 +76,30 @@ extension DocumentScanningViewController: VNDocumentCameraViewControllerDelegate
 
 extension DocumentScanningViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            let scanner = VisionKeywordScanner()
-            scanner.recognize(source: pickedImage) { (candidate, error) in
+        
+        resultsViewController = storyboard?.instantiateViewController(withIdentifier: DocumentScanningViewController.otherContentsIdentifier) as? (UIViewController & RecognizedTextDataSource)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        
+        self.activityIndicator.startAnimating()
+        
+        DispatchQueue.global(qos: .userInitiated).async  {
+            self.scanner.recognize(source: image) { (candidate, error) in
                 if error != nil {
                     print("Error: \(error?.localizedDescription ?? "")")
                 }
+                
                 DispatchQueue.main.async {
                     if let resultsVC = self.resultsViewController {
+                        picker.dismiss(animated: true, completion: nil)
                         self.resultsViewController?.addRecognizedText(recognizedText: candidate)
                         self.navigationController?.pushViewController(resultsVC, animated: true)
                     }
                     self.activityIndicator.stopAnimating()
                 }
             }
-            picker.dismiss(animated: true, completion: nil)
         }
     }
 }
